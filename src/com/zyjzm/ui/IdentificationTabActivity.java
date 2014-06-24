298a822fbec3ffe89a6f1b0cd8927791f0d4c968
cennanfang <cennanfang@gmail.com>
2014-06-24 12:35:48 +0800

    002

---
+++ src/com/zyjzm/ui/IdentificationTabActivity.java
@@ -0,0 +1,15 @@
+package com.zyjzm.ui;
+
+import org.study.github.test.R;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class IdentificationTabActivity extends Activity{
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_identification);
+	}
+}
---
+++ src/com/zyjzm/ui/UserCenterTabActivity.java
@@ -0,0 +1,15 @@
+package com.zyjzm.ui;
+
+import org.study.github.test.R;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class UserCenterTabActivity extends Activity{
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_favorite);
+	}
+}
