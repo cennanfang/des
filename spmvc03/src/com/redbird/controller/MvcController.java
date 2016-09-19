d6af7b2caffb58aad37173e7c1c5a933d6e5a68e
cennanfang <cennanfang@gmail.com>
2016-09-19 20:21:15 +0800

    add spmvc03.

---
+++ spmvc03/src/com/redbird/controller/MvcController.java
@@ -0,0 +1,38 @@
+package com.redbird.controller;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/mvc")
+public class MvcController {
+
+	@RequestMapping("/hello")
+    public String hello(){       
+        return "hello";
+    }
+	
+	
+	@RequestMapping("/out")
+	public void outData(HttpServletRequest request,HttpServletResponse response) throws IOException {
+		response.setContentType("application/json;charset=utf-8");
+		PrintWriter out = response.getWriter();
+		String jsonStr = "{username:'²ÐÈ±µÄ¹Â¶À',password:'admin123'}";
+		out.write(jsonStr);
+	}
+	
+	@RequestMapping("/out_jsp")
+	public String outDataJsp(HttpServletRequest request,HttpServletResponse response) throws IOException {
+		response.setContentType("application/json;charset=utf-8");
+		PrintWriter out = response.getWriter();
+		String jsonStr = "{username:'²ÐÈ±µÄ¹Â¶À',password:'admin123'}";
+		out.write(jsonStr);
+		return "out";
+	}
+}
