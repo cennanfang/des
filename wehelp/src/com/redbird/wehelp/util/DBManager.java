d6689750c92bf60d21f895cb0da28ebf397ac72e
cennanfang <cennanfang@gmail.com>
2016-09-23 20:51:03 +0800

    messagedao.xml测试通过

--- wehelp/src/com/redbird/wehelp/util/DBManager.java
+++ wehelp/src/com/redbird/wehelp/util/DBManager.java
@@ -4,6 +4,7 @@ import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 import com.redbird.wehelp.dao.MessageDao;
+import com.redbird.wehelp.pojo.BasePojo;
 import com.redbird.wehelp.pojo.Message;
 
 /**
@@ -18,34 +19,22 @@ public class DBManager {
 		ApplicationContext ctx=null;
         ctx=new ClassPathXmlApplicationContext("applicationContext.xml");
         MessageDao messageDao = (MessageDao) ctx.getBean("messageDao");
-        Message msg = messageDao.find(1);
-        System.out.println(msg);
-//        User user=new User();
-        //添加两条数据
-//        user = generateUser();
-//        userDao.add(user);
-//        System.out.println("添加成功");
-//        //查询数据
-//        System.out.println(userDao.find("B2B2CC32030842C18E310FA39A8D33F3").toString());
-//        System.out.println(userDao.find("387BE1A242244CB7BA1B898BBCEA2255").toString());
-//        //修改数据
-//        user = userDao.find("387BE1A242244CB7BA1B898BBCEA2255");
-//        user.setSex(2);;
-//        userDao.update(user);
-//        System.out.println("修改成功");
-//        //删除数据
-//        userDao.delete("B2B2CC32030842C18E310FA39A8D33F3");
-//        System.out.println("删除成功");
+//        MessageTypeDao messageTypeDao = (MessageTypeDao) ctx.getBean("messageTypeDao");
+//        UserDao userDao = (UserDao) ctx.getBean("userDao");
+//        Message msg = (Message)generateBasePojo();
+//        msg.setUser(userDao.find(2));
+//        msg.setMessageType(messageTypeDao.find(2));
+        Message msg = messageDao.find(3);
+        msg.setContacts("0859-4323889");
+        messageDao.update(msg);
+//        Message msg = messageDao.find(1);
+//        System.out.println(msg);
 	}
 	
-//	public static User generateUser() {
-//		User u = new User();
-//		u.setId(GUID.generate());
-//		u.setUserName("jack");
-//		u.setPassword("jack123");
-//		u.setNickName("杰克");
-//		u.setSex(1);
-//		u.setAge(18);
-//		return u;
-//	}
+	public static BasePojo generateBasePojo() {
+		Message b = new Message();
+		b.setContent("本人要从册亨兴义");
+		b.setContacts("123123132342432");
+		return b;
+	}
 }
