f9e645bea5b3ffbbfcda38f2d28ba66c1a08a076
cennanfang <cennanfang@gmail.com>
2016-11-23 17:58:51 +0800

    成功返回json窜

--- wehelp01/src/main/java/com/redbird/wehelp/web/controller/MessageController.java
+++ wehelp01/src/main/java/com/redbird/wehelp/web/controller/MessageController.java
@@ -1,17 +1,65 @@
 package com.redbird.wehelp.web.controller;
 
+import java.io.IOException;
+import java.sql.Timestamp;
+import java.text.ParseException;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import com.redbird.wehelp.pojo.MessagesModel;
+import com.redbird.wehelp.service.MessageService;
+import com.redbird.wehelp.utils.DataUtils;
 
 /**
  * 
  * @author cennanfang
  *
  */
+@Controller
 @RequestMapping("/msg")
 public class MessageController {
+	@Autowired
+	private MessageService messageService;
 
-	@RequestMapping("/loadMsgs")
-	public String loadMessages() {
-		return "message";
+	@ResponseBody
+	@RequestMapping("/refresh")
+	public String refreshMessages() throws Exception {
+		String json = "{}";
+		try {
+			Timestamp markPublishedDate = DataUtils.stringToTimesamp("2016-11-21 11:08:06.128");
+			MessagesModel msgModel = messageService.refreshMessage(markPublishedDate, 3);
+			json = DataUtils.pojoToJson(msgModel);
+		} catch (IOException | ParseException e) {
+			e.printStackTrace();
+			throw e;
+		}
+		return json;
+	}
+	
+	@ResponseBody
+	@RequestMapping("/load")
+	public String loadMessages() throws Exception {
+		String json = "{}";
+		try {
+			Timestamp maxPuhlishedDate = DataUtils.stringToTimesamp("2016-11-21 11:08:06.151");
+			Timestamp minPuhlishedDate = DataUtils.stringToTimesamp("2016-11-21 11:08:06.134");
+			MessagesModel msgModel = messageService.loadMessage(maxPuhlishedDate, minPuhlishedDate, 3);
+			json = DataUtils.pojoToJson(msgModel);
+		} catch (IOException | ParseException e) {
+			e.printStackTrace();
+			throw e;
+		}
+		return json;
+	}
+
+	public MessageService getMessageService() {
+		return messageService;
+	}
+
+	public void setMessageService(MessageService messageService) {
+		this.messageService = messageService;
 	}
 }
