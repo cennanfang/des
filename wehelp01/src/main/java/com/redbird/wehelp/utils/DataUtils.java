a6032e8ba59f21839efee37302ceeeca50ef9535
cennanfang <cennanfang@gmail.com>
2016-11-23 09:45:00 +0800

    修改DataUtils，添加json转换功能。

--- wehelp01/src/main/java/com/redbird/wehelp/utils/DataUtils.java
+++ wehelp01/src/main/java/com/redbird/wehelp/utils/DataUtils.java
@@ -4,16 +4,11 @@ import java.io.IOException;
 import java.sql.Timestamp;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
-import java.util.ArrayList;
-import java.util.Date;
-import java.util.List;
 
 import com.fasterxml.jackson.core.JsonParseException;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.JsonMappingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
-import com.redbird.wehelp.pojo.Message;
-import com.redbird.wehelp.pojo.MessagesModel;
 
 public class DataUtils {
 	/**
@@ -55,16 +50,13 @@ public class DataUtils {
 		ObjectMapper objectMapper = new ObjectMapper();
 		String json = objectMapper.writeValueAsString(pojo);
 		return json;
-		
 	}
 	
 	/**
 	 * 将对象转换为json串
 	 * @param <T>
 	 * @param o
-	 * @return 
-	 * @return 
-	 * @return
+	 * @return T
 	 * @throws IOException 
 	 * @throws JsonMappingException 
 	 * @throws JsonParseException 
@@ -73,42 +65,5 @@ public class DataUtils {
 		ObjectMapper objectMapper = new ObjectMapper();
 		T pojo = objectMapper.readValue(json, c);
 		return pojo;
-		
-	}
-	
-	public static void main(String[] args) {
-		Message m = new Message();
-		m.setId(1);
-		m.setContacts("00090192");
-		m.setContent("从贵阳的必经");
-		m.setCreateDate(new Timestamp(new Date().getTime()));
-		m.setPublishedDate(new Timestamp(new Date().getTime()));
-		m.setMessageTypeId(1);
-		m.setUserId(1);
-		Message m2 = new Message();
-		m2.setId(2);
-		m2.setContacts("asdfasdfa");
-		m2.setContent("纽约到伦敦");
-		m2.setCreateDate(new Timestamp(new Date().getTime()));
-		m2.setPublishedDate(new Timestamp(new Date().getTime()));
-		m2.setMessageTypeId(2);
-		m2.setUserId(2);
-		List<Message> ms = new ArrayList<Message>();
-		ms.add(m);
-		ms.add(m2);
-		MessagesModel mm = new MessagesModel();
-		mm.setMarkPublishedDate(new Timestamp(new Date().getTime()));
-		mm.setMessages(ms);
-		mm.setMesgsTotal(2);
-		
-		try {
-			String json = DataUtils.pojoToJson(mm);
-			System.out.println(json);
-			
-			MessagesModel msgM = DataUtils.jsonToPojo(json, MessagesModel.class);
-			System.out.println(msgM);
-		} catch (ParseException | IOException e) {
-			e.printStackTrace();
-		}
 	}
 }
